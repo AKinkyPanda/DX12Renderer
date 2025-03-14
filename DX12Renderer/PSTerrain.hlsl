681752d7f4ba643d07566147847106a32e047e9d
Sava Terzeesky <sava.terzeesky@yahoo.com>
2025-03-14 12:28:36 +0100

    # [ADDED] Debug returns for color
    Testing LODs

--- DX12Renderer/PSTerrain.hlsl
+++ DX12Renderer/PSTerrain.hlsl
@@ -18,6 +18,15 @@ float4 main(PixelShaderInput input) : SV_TARGET
     float diffuse = saturate(dot(input.Normal, light));
     float ambient = 0.1f;
     float3 color = float3(0.32f, 0.82f, 0.41f);
+
+    //float4 camPos = float4(-4300, 2000, 1500, 1);
+    //float d = distance(input.WorldPos.xyz, camPos);
+    //return float4(saturate(d / 2000.0f), saturate(d / 2000.0f), saturate(d / 2000.0f), 1.0);
+
+    //return float4(input.WorldPos.y * 0.001, input.WorldPos.y * 0.001, input.WorldPos.y * 0.001, 1.0);
+
+    //float4 debugColor = float4(input.WorldPos.xyz * 0.001, 1.0);  // Scale down for visibility
+    //return debugColor;
      
     return float4(saturate((color * diffuse) + (color * ambient)), 1.0f);
 }
