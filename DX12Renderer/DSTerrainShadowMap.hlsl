da233ede22c55b89e56f5c15bcb0d7dbfc5d36ad
Sava Terzeesky <sava.terzeesky@yahoo.com>
2025-06-13 10:52:50 +0200

    # [ADDED] Hull and domain shaders for terrain shadow map

---
+++ DX12Renderer/DSTerrainShadowMap.hlsl
@@ -0,0 +1,89 @@
+struct Mat
+{
+    matrix ModelMatrix;
+    matrix ModelViewMatrix;
+    matrix InverseTransposeModelViewMatrix;
+    matrix ModelViewProjectionMatrix;
+};
+
+cbuffer MatCB : register(b2)
+{
+    Mat Matrices;
+}
+
+cbuffer cbLightViewProj : register(b0)
+{
+    matrix gLightViewProj;
+};
+
+Texture2D<float4> heightmap : register(t2);
+
+SamplerState hmsampler : register(s0);
+
+struct DS_OUTPUT
+{
+    float4 pos : SV_POSITION;
+    float4 WorldPos : POSITION1;
+    float4 ShadowPos : POSITION2;
+    float4 norm : NORMAL;
+    float2 UV : TEXCOORD;
+    float3 FragPos : COLOR0;
+};
+ 
+// Output control point
+struct HS_CONTROL_POINT_OUTPUT
+{
+    float4 pos : POSITION0;
+    float4 norm : NORMAL;
+    float2 UV : TEXCOORD;
+    float3 FragPos : COLOR0;
+};
+ 
+// Output patch constant data.
+struct HS_CONSTANT_DATA_OUTPUT
+{
+    float EdgeTessFactor[4]         : SV_TessFactor; // e.g. would be [4] for a quad domain
+    float InsideTessFactor[2]          : SV_InsideTessFactor; // e.g. would be Inside[2] for a quad domain
+};
+ 
+#define NUM_CONTROL_POINTS 4
+ 
+[domain("quad")]
+DS_OUTPUT main(
+    HS_CONSTANT_DATA_OUTPUT input,
+    float2 domain : SV_DomainLocation,
+    const OutputPatch<HS_CONTROL_POINT_OUTPUT, NUM_CONTROL_POINTS> patch)
+{
+    DS_OUTPUT output;
+
+    float2 bottomUV = lerp(patch[0].UV, patch[1].UV, domain.x);
+    float2 topUV    = lerp(patch[2].UV, patch[3].UV, domain.x);
+    float2 interpolatedUV = lerp(bottomUV, topUV, domain.y);
+
+    // ***changed from barycentric to (u,v) coordinates. Now need to use bilinear interpolation to find the new points.
+    float4 bottom = lerp(patch[0].pos, patch[1].pos, domain.x);
+    float4 top    = lerp(patch[2].pos, patch[3].pos, domain.x);
+    float4 interpolatedWorldPos = lerp(bottom, top, domain.y);
+
+    // Remove hardcoded values
+    float scale = 1024.0f / 4.0f;
+    float displacement = heightmap.SampleLevel(hmsampler, interpolatedUV, 0.0).r;
+    interpolatedWorldPos.y += displacement * scale;
+    
+    output.WorldPos = interpolatedWorldPos;
+     
+    output.pos = output.WorldPos;
+    output.pos = mul(Matrices.ModelViewProjectionMatrix, output.pos);
+
+    float4 shadowPos = mul(output.WorldPos, Matrices.ModelMatrix);
+    output.ShadowPos = float4(shadowPos.xyz, 1);
+
+    output.FragPos = mul( Matrices.ModelViewMatrix, interpolatedWorldPos);
+
+    output.norm = lerp(lerp(patch[0].norm, patch[1].norm, domain.x), lerp(patch[2].norm, patch[3].norm, domain.x), domain.y);
+    output.UV = interpolatedUV;
+
+    output.pos = mul(interpolatedWorldPos, gLightViewProj);
+
+    return output;
+}
---
+++ DX12Renderer/HSTerrainShadowMap.hlsl
@@ -0,0 +1,90 @@
+cbuffer CameraPosition : register(b3)
+{
+    float4 camPos;
+}
+
+// Input control point
+struct VS_OUTPUT
+{
+    float4 pos : SV_POSITION;
+    float4 WorldPos : POSITION1;
+    float4 ShadowPos : POSITION2;
+    float4 norm : NORMAL;
+    float2 UV : TEXCOORD;
+    float3 FragPos : COLOR0;
+};
+
+// Output control point
+struct HS_CONTROL_POINT_OUTPUT
+{
+    float4 pos : POSITION0;
+    float4 norm : NORMAL;
+    float2 UV : TEXCOORD;
+    float3 FragPos : COLOR0;
+};
+ 
+// Output patch constant data.
+struct HS_CONSTANT_DATA_OUTPUT
+{
+    float EdgeTessFactor[4]         : SV_TessFactor; // e.g. would be [4] for a quad domain
+    float InsideTessFactor[2]          : SV_InsideTessFactor; // e.g. would be Inside[2] for a quad domain
+};
+ 
+#define NUM_CONTROL_POINTS 4
+
+// We add the following function to calculate what tessellation factor to use at
+// what distance. Factors used are all factors of 2. ie 1, 2, 4, 8, 16, 32, 64 (64 being max)
+// because we're using [partitioning("fractional_even")], the tessellator should automatically
+// interpolate between these values, taking care of potential popping concerns.
+float CalcTessFactor(float3 p) {
+    float d = distance(p, camPos);
+ 
+    float s = saturate((d - 16.0f) / (512.0f - 16.0f));
+    return pow(2, (lerp(6, 0, s)));
+}
+ 
+// Patch Constant Function
+HS_CONSTANT_DATA_OUTPUT CalcHSPatchConstants(
+    InputPatch<VS_OUTPUT, NUM_CONTROL_POINTS> ip,
+    uint PatchID : SV_PrimitiveID)
+{
+    HS_CONSTANT_DATA_OUTPUT output;
+ 
+    // tessellate based on distance from the camera.
+    // compute tess factor based on edges.
+    // compute midpoint of edges.
+    float3 e0 = 0.5f * (ip[0].WorldPos + ip[2].WorldPos);
+    float3 e1 = 0.5f * (ip[0].WorldPos + ip[1].WorldPos);
+    float3 e2 = 0.5f * (ip[1].WorldPos + ip[3].WorldPos);
+    float3 e3 = 0.5f * (ip[2].WorldPos + ip[3].WorldPos);
+    float3 c = 0.25f * (ip[0].WorldPos + ip[1].WorldPos + ip[2].WorldPos + ip[3].WorldPos);
+ 
+    output.EdgeTessFactor[0] = CalcTessFactor(e0);
+    output.EdgeTessFactor[1] = CalcTessFactor(e1);
+    output.EdgeTessFactor[2] = CalcTessFactor(e2);
+    output.EdgeTessFactor[3] = CalcTessFactor(e3);
+    output.InsideTessFactor[0] = CalcTessFactor(c);
+    output.InsideTessFactor[1] = output.InsideTessFactor[0];
+ 
+    return output;
+}
+ 
+[domain("quad")]
+[partitioning("fractional_even")]
+[outputtopology("triangle_cw")]
+[outputcontrolpoints(4)]
+[patchconstantfunc("CalcHSPatchConstants")]
+HS_CONTROL_POINT_OUTPUT main( 
+    InputPatch<VS_OUTPUT, NUM_CONTROL_POINTS> ip, 
+    uint i : SV_OutputControlPointID,
+    uint PatchID : SV_PrimitiveID )
+{
+    HS_CONTROL_POINT_OUTPUT output;
+ 
+    // Insert code to compute Output here
+    output.pos = ip[i].pos;
+    output.norm = ip[i].norm;
+    output.UV = ip[i].UV;
+ 
+    return output;
+}
